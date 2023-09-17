a4f242eef8e70cbdc3db6b4764e93ce661d95920
Patrick Doane <pdoane2@gmail.com>
2023-09-16 18:58:43 -0700

    Initial import

---
+++ build.zig
@@ -0,0 +1,18 @@
+const std = @import("std");
+
+pub fn build(b: *std.Build) void {
+    const target = b.standardTargetOptions(.{});
+    const optimize = b.standardOptimizeOption(.{});
+
+    const exe = b.addExecutable(.{
+        .name = "mach-objc-generator",
+        .root_source_file = .{ .path = "src/main.zig" },
+        .target = target,
+        .optimize = optimize,
+    });
+    b.installArtifact(exe);
+
+    const run_cmd = b.addRunArtifact(exe);
+    const run_step = b.step("run", "Run the app");
+    run_step.dependOn(&run_cmd.step);
+}
---
+++ headers.m
@@ -0,0 +1,4 @@
+// clang headers.m -Xclang -ast-dump=json -fsyntax-only > headers.json
+
+#include <Metal/Metal.h>
+
